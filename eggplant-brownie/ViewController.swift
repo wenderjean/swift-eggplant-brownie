7c5b1a01046932a9a7460c677e83f9751a6971d5
Wender Freese <wender.jean@gmail.com>
2016-05-15 10:26:35 -0300

    First Commit, this version includes a first storyboard view as example.

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nameField : UITextField!
+    @IBOutlet var happinessField : UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction
+    func add() {
+        let name = nameField.text
+        let happiness = happinessField.text
+        println("eaten: \(name) \(happiness)!")
+    }
 }
 
