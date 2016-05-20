b5417492619498063c9476264163e8bd31990589
Wender Freese <wender.jean@gmail.com>
2016-05-20 18:47:48 -0300

    Adding support to user can select itens when saving a meal

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -12,22 +12,53 @@ protocol AddMealDelegate {
     func add(meal: Meal)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var nameField : UITextField!
     @IBOutlet var happinessField : UITextField!
     var delegate:AddMealDelegate?
+    
+    var items = [
+        Item(name: "Eggplant Brownie", calories: 10),
+        Item(name: "Zuchini Muffim", calories: 10),
+        Item(name: "Cookies", calories: 10),
+        Item(name: "Coconut Oil", calories: 500),
+        Item(name: "Chocolat Frosting", calories: 1000),
+        Item(name: "Chocolat Chip", calories: 1000)
+    ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var selected = Array<Item>()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        cell.textLabel?.text = item.name
+        return cell
     }
-
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        
+        if cell == nil {
+            return
+        }
+        
+        if cell?.accessoryType == UITableViewCellAccessoryType.None {
+            cell?.accessoryType = UITableViewCellAccessoryType.Checkmark
+            selected.append(items[indexPath.row])
+        } else {
+            cell?.accessoryType = UITableViewCellAccessoryType.None
+            if let position = find(selected, items[indexPath.row]) {
+                selected.removeAtIndex(position)
+            }
+        }
+    }
+    
     @IBAction
     func add() {
         if nameField == nil || happinessField == nil {
@@ -42,6 +73,7 @@ class ViewController: UIViewController {
         }
         
         let meal = Meal(name: name, happiness: happiness!)
+        meal.items = selected
         
         
         if delegate == nil {
